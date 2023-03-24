6cf26105c9c22babf33158a3a221d909e83af852
CanOpik <canula2000@gmail.com>
2023-03-24 10:38:48 +0300

    .

--- app/src/main/java/com/example/mobproject/SignUpActivity.java
+++ app/src/main/java/com/example/mobproject/SignUpActivity.java
@@ -6,7 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -14,10 +16,15 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        ImageView imageView = (ImageView)findViewById(R.id.imageView2);
     }
 
     public void onClick(View view) {
         Intent intent = new Intent(this, SignIn.class);
         startActivity(intent);
+    }
+    public void signupClick(View view){
+        Toast toast = Toast.makeText(getApplicationContext(), "Click", Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
