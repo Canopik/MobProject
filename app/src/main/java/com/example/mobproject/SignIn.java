a400983a650fffc9f586411f46983ee0a7e41208
CanOpik <canula2000@gmail.com>
2023-03-24 22:37:38 +0300

    .

--- app/src/main/java/com/example/mobproject/SignIn.java
+++ app/src/main/java/com/example/mobproject/SignIn.java
@@ -13,35 +13,23 @@ import android.widget.EditText;
 
 public class SignIn extends AppCompatActivity {
     private static final String TAG = "MyApplication";
-    private EditText email;
-    private EditText pass;
+    private EditText login;
+    private EditText password;
     private Button signin2_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
-        EditText signin = (EditText)findViewById(R.id.signin_text);
-        EditText pass = (EditText)findViewById(R.id.pass);
         Button signin2_button = (Button)findViewById(R.id.signin2_button);
+        login = (EditText)findViewById(R.id.signin_text);
+        password = (EditText)findViewById(R.id.pass);
         Intent intent_signup = new Intent(this, SignUpActivity.class);
-        signin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                intent_signup.putExtra("email_name_key", email.getText().toString());
-                startActivity(intent_signup);
-            }
-        });
-        Intent intent_pass = new Intent(this, SignUpActivity.class);
-        pass.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                intent_pass.putExtra("pass_key", pass.getText().toString());
-            }
-        });
         signin2_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent signup_intent = new Intent(getApplicationContext(), SignUpActivity.class);
+                intent_signup.putExtra("login", login.getText().toString());
+                intent_signup.putExtra("password", password.getText().toString());
                 startActivity(signup_intent);
             }
         });
--- app/src/main/java/com/example/mobproject/SignUpActivity.java
+++ app/src/main/java/com/example/mobproject/SignUpActivity.java
@@ -2,23 +2,35 @@ package com.example.mobproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
 public class SignUpActivity extends AppCompatActivity {
 
+    private TextView login;
+    private TextView password;
+
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        ImageView imageView = (ImageView)findViewById(R.id.imageView2);
+        login = (TextView) findViewById(R.id.editTextTextEmailAddress);
+        password = (TextView)findViewById(R.id.editTextTextPassword);
+        String txtLogin = getIntent().getStringExtra("login");
+        String txtPassword = getIntent().getStringExtra("password");
+        login.setText(login.getText().toString() + txtLogin);
+        password.setText(password.getText().toString() + txtPassword);
     }
-
     public void onClick(View view) {
         Intent intent = new Intent(this, SignIn.class);
         startActivity(intent);
